e7f7ca49ad2c6934639d787ba5f5bb9482be576e
rjain37 <72268614+rjain37@users.noreply.github.com>
2021-04-11 16:19:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function checkForValidity(){
 	var site = getParameterByName('NewsInput');
-	var signature = site.substring(site.indexOf("."), site.indexOf(".") + 4);
+	var index = getPosition(site, ".", 2);
+	var signature = site.substring(index, index + 4);
 	var finalMessage;
 	if(signature === '.gov' || signature === '.org' || signature === '.edu') {
 		finalMessage = "Amazing! This website, " + site + ", is trustworthy.";
@@ -18,3 +19,6 @@ function getParameterByName(name, url = window.location.href) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }//source: https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function getPosition(string, subString, index) {
+  return string.split(subString, index).join(subString).length;
+}//source: https://stackoverflow.com/questions/14480345/how-to-get-the-nth-occurrence-in-a-string
