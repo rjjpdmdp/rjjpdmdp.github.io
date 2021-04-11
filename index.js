19d204d23bd9cf2415312ce48fd08afff832e322
rjain37 <72268614+rjain37@users.noreply.github.com>
2021-04-11 16:05:20 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+function checkForValidity(){
+	var site = getParameterByName('NewsInput');
+	console.log(site);
+	var signature = site.substring(site.length-4);
+	var finalMessage;
+	if(signature === '.gov' || signature === '.org' || signature === '.edu') {
+		finalMessage = "Amazing! This website is trustworthy.";
+	}else{
+		finalMessage = "Oh no! You might want to get your information somewhere else. This website may not be the most trustworthy."
+	}
+	document.getElementById("result").innerHTML = finalMessage;
+}
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}//source: https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
